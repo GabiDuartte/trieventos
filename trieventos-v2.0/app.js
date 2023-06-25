e85cc6cf041379cd49f4c5d5fa1c1d3f389ac370
Gabriela Albornoz <93677092+GabiDuartte@users.noreply.github.com>
2023-06-25 14:59:34 -0300

    Update app.js

--- trieventos-v2.0/app.js
+++ trieventos-v2.0/app.js
@@ -3,44 +3,109 @@ const ejs = require('ejs');
 const path = require('path');
 const {Pool} = require('pg');
 const jwt = require('jsonwebtoken');
-//const expressJwt = require('express-jwt');
-//const bcrypt = require('bcryptjs');
+const expressJwt = require('express-jwt');
+const bcrypt = require('bcryptjs');
+const match = require('assert');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
+const chave = '3eventsxcz';
 
 const app = express();
 
+app.set('view engine', 'ejs');
+
+
+app.use(express.json());
+//para servir arquivos estáticos ao CSS
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cookieParser());
+
 const pool = new Pool(
     {
-        user: '',
-        password: '',
-        host: '',
+        user: 'postgres',
+        password: 'bts',
+        host: 'localhost',
         port: 5432,
-        database: ''
+        database: 'postagem'
     }
 );
 
-function generateToken(user) {
-  if (!user || !user.id_usuario) {
-    throw new Error('Propriedade id_usuario não fornecida');
-  }
 
-  const token = jwt.sign({ id: user.id_usuario, role: user.role }, secretKey, { expiresIn: '1h' });
+function generateToken(user) {
+  const token = jwt.sign({ id: user.id, tipo: user.tipo }, chave, { expiresIn: '1h' });
   return token;
 }
-  
-const secretKey = 'chave-secreta';
 
-  app.set('view engine', 'ejs');
   
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.static(path.join(__dirname, 'public')));
+function authenticateToken(req, res, next) {
+  const token = req.cookies.token;
+
+  if (!token) {
+    return res.redirect('/login');
+  }
+
+  try {
+    const decoded = jwt.verify(token, chave);
+    req.user = decoded;
+
+    // Verificar se o usuário é do tipo 'usuario' ou 'estabelecimento'
+    if (decoded.tipo !== 'usuario' && decoded.tipo !== 'estabelecimento') {
+      return res.redirect('/login');
+    }
+
+    next();
+  } catch (error) {
+    return res.redirect('/login');
+  }
+}
   
+function authenticateTokenEstabelecimentos(req, res, next) {
+    const token = req.cookies.token;
+  
+    if (!token) {
+      return res.redirect('/login-estabelecimentos');
+    }
+  
+    try {
+      const decoded = jwt.verify(token, chave);
+      req.user = decoded;
+  
+      if (decoded.tipo_usuario !== 'estabelecimento') {
+        return res.redirect('/login-estabelecimentos');
+      }
+  
+      next();
+    } catch (error) {
+      return res.redirect('/login-estabelecimentos');
+    }
+  }
 
+/*
+function authenticateTokenU(req, res, next) {
+    const token = req.cookies.token;
+  
+    if (!token) {
+      return res.redirect('/login');
+    }
+  
+    try {
+      const decoded = jwt.verify(token, chave);
+      req.user = decoded;
+  
+      if (decoded.tipo && decoded.tipo === 'usuario') {
+        return res.redirect('/login');
+      }
+  
+      next();
+    } catch (error) {
+      return res.redirect('/login');
+    }
+  }
+*/
 
+  
 app.get('/', async (req, res) => {
     //console.log(res.json);
     try {
@@ -70,14 +135,12 @@ app.get('/estabelecimentos/x', function(req,res){
 app.get('/estabelecimentos/:nome_estabelecimento', async (req,res) => {
     const nome = req.params.nome_estabelecimento;
     try {
-        const result = await pool.query(`select * from estabelecimentos where titulo_card = '${nome}'`);
+        const result = await pool.query(`select * from estabelecimentos where estabelecimento_nome = '${nome}'`);
         const dadosObtidos = result.rows;
         const titleTag = dadosObtidos[0].titulo_card;
         //console.log(titleTag);
         //console.log(dadosObtidos);
-      const ocupados = await pool.query('SELECT * FROM horarios WHERE id_estabelecimento = ${dadosObtidos[0].id_estabelecimentos AND status = false');
-
-        res.render('usuarios-comuns/render-estabelecimentos.ejs', {titleTag: titleTag, estabelecimento: dadosObtidos, ocupados: ocupados.rows});
+        res.render('usuarios-comuns/render-estabelecimentos.ejs', {titleTag: titleTag, estabelecimento: dadosObtidos});
     } catch (error) {
         console.error('Erron ao buscar dados: ', error);
         res.sendStatus(500).send('Erro Interno');
@@ -100,7 +163,7 @@ app.get('/login', function(req, res){
 });
 
 //login de usuário comum
-app.get('/login-locatarios', async(req, res) => {
+app.get('/login-locatorio', async(req, res) => {
     res.render('login/login-usuarios', {titleTag: 'Login'});
 });
 
@@ -110,18 +173,13 @@ app.get('/login-estabelecimentos', async(req, res) => {
 
 });
 
-
-
-app.get('/perfil', function(req, res){
-  res.render('perfil', {titleTag: 'Perfil'});
-
+app.get('/perfil', authenticateToken, function(req, res){
+    console.log(req.id + 'chamou a rota');
+    res.render('perfil', {titleTag: 'Perfil'});
 });
-
 
 app.get('/cadastrar-usuario', function(req, res){
     res.render('cadastro-usuario', {titleTag: 'Cadastrar Usuário'});
-
-    window.addEventListener('DOMContentLoaded', fetchProfileData);
 });
 
 app.get('/cadastrar-estabelecimento', function(req, res){
@@ -132,144 +190,182 @@ app.get('/redefinir-senha', function(req, res){
     res.render('redefinir-senha', {titleTag: 'Redefinir Senha'});
 })
 
-app.get('/x/gerenciar', function(req, res){
-  res.render('alterar-perfil', {titleTag: 'Alterar Perfil'});
+app.get('/x/gerenciar', authenticateToken,  function(req, res){
+    res.render('alterar-perfil', {titleTag: 'Alterar Perfil'});
 })
 
-app.get('/x/locacoes', function(req, res){
-  res.render('locacoes', {titleTag: 'Histórico de Locações'});
+app.get('/x/locacoes', authenticateToken, function(req, res){
+    res.render('locacoes', {titleTag: 'Histórico de Locações'});
 })
+
 // Posts
 app.post('/login-locatorio', async (req, res) => {
-  const { nome, senha } = req.body;
+  const nome = req.body.nome;
+  const senha = req.body.senha;
 
+  if (!nome) {
+    return res.status(400).json({ message: 'Campo "Usuário" não preenchido' });
+  }
+
+  if (!senha) {
+    return res.status(400).json({ message: 'Campo "Senha" não preenchido' });
+  }
+  
   try {
-    const result = await pool.query('SELECT * FROM usuarios WHERE nome = $1', [nome]);
+    const result = await pool.query(`SELECT * FROM usuarios WHERE usuario_nome = '${nome}'`);
     const user = result.rows[0];
 
     if (!user) {
-      return res.status(401).json({ message: 'Usuário Inválido' });
+      return res.status(404).json({ message: 'Usuário não possui cadastro' });
     }
 
-    // Comparar a senha fornecida com a senha armazenada no banco de dados
-    if (senha !== user.senha) {
-      return res.status(401).json({ message: 'Dados Inválidos' });
+    const compararSenha = await bcrypt.compare(senha, user.usuario_senha);
+
+    if (!compararSenha) {
+      return res.status(401).json({ message: 'Credenciais Inválidas' });
     }
 
-    const token = jwt.sign({ id: user.id_usuario, tipo: 'usuario' }, 'chave-secreta');
+    const token = jwt.sign({ id: user.usuario_id, tipo: 'usuario' }, chave, { expiresIn: 1800 });
 
-    // Redirecionar para a página principal
-    res.redirect('/');
+    res.cookie('token', token, { maxAge: 1800000, httpOnly: true });
+    res.redirect('/perfil');
   } catch (error) {
     console.error('Erro de autenticação', error);
     res.sendStatus(500);
   }
 });
 
+
+//gets de estabelecimentos
 app.post('/login-estabelecimentos', async (req, res) => {
   const { nome, senha } = req.body;
 
   try {
-    // Verificar se o estabelecimento está cadastrado
-    const result = await pool.query('SELECT * FROM usuarios_estabelecimentos WHERE nome = $1', [nome]);
+    const result = await pool.query(`SELECT * FROM estabelecimentos WHERE estabelecimento_nome = $1`, [nome]);
     const estabelecimento = result.rows[0];
 
     if (!estabelecimento) {
       return res.status(401).json({ message: 'Credenciais Inválidas' });
     }
 
-    if (senha !== estabelecimento.senha) {
-      return res.status(401).json({ message: 'Dados Inválidos' });
-    }
+    const match = await bcrypt.compare(senha, estabelecimento.estabelecimento_senha);
 
-    const token = jwt.sign({ id: estabelecimento.id_usuario_e, tipo: 'estabelecimento' }, 'chave-secreta');
+    if (match) {
+      const token = jwt.sign({ id: estabelecimento.estabelecimento_id, tipo_usuario: 'estabelecimento' }, chave, { expiresIn: '1h' });
 
-    res.redirect('/');
-  } catch (error) {
-    console.error('Erro ao autenticar estabelecimento', error);
-    res.sendStatus(500);
-  }
-});
-
-app.post('/local', async (req, res) => {
-  const { nome, senha } = req.body;
-
-  try {
-    // Verificar se o estabelecimento está cadastrado
-    const result = await pool.query('SELECT * FROM usuarios_estabelecimentos WHERE nome = $1', [nome]);
-    const estabelecimento = result.rows[0];
-
-    if (!estabelecimento) {
+      res.cookie('token', token, { maxAge: 3600000, httpOnly: true });
+      res.redirect('/local');
+    } else {
       return res.status(401).json({ message: 'Credenciais Inválidas' });
     }
-
-   
-    
-    if (senha !== estabelecimento.senha) {
-      return res.status(401).json({ message: 'Dados Inválidos' });
-    }
-    
-    const token = jwt.sign({ id: estabelecimento.id_usuario_e, tipo: 'estabelecimento' }, 'chave-secreta');
-      
-      // Redirecionar para a página principal
-    res.redirect('/');
-    
   } catch (error) {
     console.error('Erro ao autenticar estabelecimento', error);
     res.sendStatus(500);
   }
 });
 
-app.post('/cadastrar-usuario', function(req, res) {
-  const { nome, email, senha, confirmarSenha } = req.body;
+app.get('/local', authenticateTokenEstabelecimentos, function(req, res) {
+  res.render('estabelecimentos/home', { titleTag: 'Home' });
+});
 
-  // Verificar se todos os campos foram preenchidos
-  if (!nome || !email || !senha || !confirmarSenha) {
-    return res.status(400).json({ message: 'Todos os campos devem ser preenchidos' });
-  }
 
-  // Verificar se a senha e a confirmação de senha são iguais
-  if (senha !== confirmarSenha) {
-    return res.status(400).json({ message: 'A senha e a confirmação de senha não correspondem' });
-  }
+app.post('/logout', function(req, res) {
+  // Limpar o cookie de token
+  res.clearCookie('token');
+  
+  res.redirect('/');
+});
 
-  // Verificar se o usuário já está cadastrado (exemplo com consulta no banco de dados)
-  const query = 'SELECT * FROM usuarios WHERE nome = $1';
-  const values = [nome];
 
-  pool.query(query, values, (error, result) => {
-    if (error) {
-      console.error('Erro ao verificar usuário no banco de dados:', error);
-      return res.status(500).json({ message: 'Erro interno' });
+//posts
+app.post('/cadastrar-usuario', async (req, res) => {
+    const nome = req.body.nome;
+    const email = req.body.email;
+    const senha = req.body.senha;
+    const confirmarSenha = req.body.confirmarSenha;
+
+    //console.log(nome, email, senha, confirmarSenha)
+    
+    // Verificar se todos os campos foram preenchidos
+    if (!nome) {
+      return res.status(400).json({ message: 'Campo "Nome de usuário" não preenchido' });
     }
 
-    if (result.rows.length > 0) {
-      return res.status(400).json({ message: 'Usuário já cadastrado' });
+    if (!email) {
+        return res.status(400).json({ message: 'Campo "E-mail" não preenchido' });
     }
 
-    // Cadastrar o usuário (exemplo com inserção no banco de dados)
-    const insertQuery = 'INSERT INTO usuarios (nome, email, senha) VALUES ($1, $2, $3)';
-    const insertValues = [nome, email, senha];
+    if (!senha) {
+        return res.status(400).json({ message: 'Campo "Senha" não preenchido' });
+    }
+    if (!confirmarSenha) {
+        return res.status(400).json({ message: 'Campo "Confirmar Senha" não preenchido' });
+    }
+    // Verificar se a senha e a confirmação de senha são iguais
+    if (senha !== confirmarSenha) {
+      return res.status(400).json({ message: 'A senha e a confirmação de senha não correspondem' });
+    }
+  
+    const hashSenha = await bcrypt.hash(senha, 10);
 
-    pool.query(insertQuery, insertValues, (insertError, insertResult) => {
-      if (insertError) {
-        console.error('Erro ao cadastrar usuário no banco de dados:', insertError);
+    // Verificar se o usuário já está cadastrado (exemplo com consulta no banco de dados)
+    const query = `SELECT * FROM usuarios WHERE usuario_nome = '${nome}'`;
+  
+    pool.query(query, (error, result) => {
+      if (error) {
+        console.error('Erro ao verificar usuário no banco de dados:', error);
         return res.status(500).json({ message: 'Erro interno' });
       }
-
-      //res.status(200).json({ message: 'Usuário cadastrado com sucesso' });
-      res.redirect('/login');
+  
+      if (result.rows.length > 0) {
+        return res.status(400).json({ message: 'Usuário já cadastrado' });
+      }
+  
+      // Cadastrar o usuário (exemplo com inserção no banco de dados)
+      const insertQuery = `INSERT INTO usuarios (usuario_nome, usuario_email, usuario_senha) VALUES ('${nome}', '${email}', '${hashSenha}')`;
+      //const insertValues = [nome, email, senha];
+  
+      pool.query(insertQuery, (insertError, insertResult) => {
+        if (insertError) {
+          console.error('Erro ao cadastrar usuário no banco de dados:', insertError);
+          return res.status(500).json({ message: 'Erro interno' });
+        }
+  
+        //res.status(200).json({ message: 'Usuário cadastrado com sucesso' });
+        res.redirect('/login')
+      });
     });
   });
-});
+  
+  app.post('/cadastrar-estabelecimento', async (req, res) => {
+    const nome = req.body.nome;
+    const email = req.body.email;
+    const senha = req.body.senha;
+    const confirmarSenha = req.body.confirmarSenha;
 
-app.post('/cadastrar-estabelecimento', function(req, res) {
-    const { nome, email, senha, confirmarSenha } = req.body;
-
+    //console.log(nome, email, senha, confirmarSenha)
+    
     // Verificar se todos os campos foram preenchidos
-    if (!nome || !email || !senha || !confirmarSenha) {
-      return res.status(400).json({ message: 'Todos os campos devem ser preenchidos' });
+    if (!nome) {
+      return res.status(400).json({ message: 'Campo "Nome de usuário" não preenchido' });
     }
+
+    if (!email) {
+        return res.status(400).json({ message: 'Campo "E-mail" não preenchido' });
+    }
+
+    if (!senha) {
+        return res.status(400).json({ message: 'Campo "Senha" não preenchido' });
+    }
+    if (!confirmarSenha) {
+        return res.status(400).json({ message: 'Campo "Confirmar Senha" não preenchido' });
+    }
+    // Verificar se a senha e a confirmação de senha são iguais
+    if (senha !== confirmarSenha) {
+      return res.status(400).json({ message: 'A senha e a confirmação de senha não correspondem' });
+    }
+  
+    const hashSenha = await bcrypt.hash(senha, 10);
 
     // Verificar se a senha e a confirmação de senha são iguais
     if (senha !== confirmarSenha) {
@@ -277,10 +373,9 @@ app.post('/cadastrar-estabelecimento', function(req, res) {
     }
 
     // Verificar se o estabelecimento já está cadastrado
-    const query = 'SELECT * FROM usuarios_estabelecimentos WHERE nome = $1';
-    const values = [nome];
+    const query = `SELECT * FROM estabelecimentos WHERE estabelecimento_nome = '${nome}'`;
 
-    pool.query(query, values, (error, result) => {
+    pool.query(query, (error, result) => {
       if (error) {
         console.error('Erro ao verificar estabelecimento no banco de dados:', error);
         return res.status(500).json({ message: 'Erro interno' });
@@ -290,10 +385,10 @@ app.post('/cadastrar-estabelecimento', function(req, res) {
         return res.status(400).json({ message: 'Estabelecimento já cadastrado' });
       }
 
-      const insertQuery = 'INSERT INTO usuarios_estabelecimentos (nome, email, senha,tipo_usuario) VALUES ($1, $2, $3,$4)';
-      const insertValues = [nome, email, senha, 'estabelecimentos'];
+      const insertQuery = `INSERT INTO estabelecimentos (estabelecimento_nome, estabelecimento_email, estabelecimento_senha, tipo_usuario) VALUES ('${nome}', '${email}', '${hashSenha}', 'estabelecimentos')`;
+      //const insertValues = [nome, email, senha, 'estabelecimentos'];
   
-      pool.query(insertQuery, insertValues, (insertError, insertResult) => {
+      pool.query(insertQuery, (insertError, insertResult) => {
         if (insertError) {
           console.error('Erro ao cadastrar usuário no banco de dados:', insertError);
           return res.status(500).json({ message: 'Erro interno' });
@@ -310,7 +405,7 @@ app.post('/', function(req, res){
     res.redirect('/');
 })
 
-app.post('/local', function(req, res){
+app.post('/local',  function(req, res){
     //aqui devemos pegar as informações de login e buscar no banco de dados
     res.redirect('/local');
 })
